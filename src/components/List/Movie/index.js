d5c3f34d2a7e3130828fd6ddba4f7031d37f913c
Maxime <trillot.maxime@outlook.fr>
2022-02-08 11:44:45 +0100

    better stars rating

--- src/components/List/Movie/index.js
+++ src/components/List/Movie/index.js
@@ -1,29 +1,13 @@
 import React, { useState } from "react";
 import api from "src/api";
 
-import { FaStar, FaStarHalf, FaTrash } from "react-icons/fa";
+import { FaStar, FaStarHalfAlt, FaTrash } from "react-icons/fa";
 
 import "./styles.scss";
 
 import PropTypes from "prop-types";
 
 const Movie = ({ movies }) => {
-  const getRating = (movie, score) => {
-    let starArray = [];
-    if (Number.isInteger(score)) {
-      for (let i = 0; i < score; i++) {
-        starArray.push(<FaStar color="yellow" key={"yellow" + i} />);
-      }
-    } else {
-      for (let i = 0; i < score - 1; i++) {
-        starArray.push(<FaStar color="yellow" key={"yellow" + i} />);
-      }
-      starArray.push(<FaStarHalf color="yellow" key={movie.id} />);
-    }
-
-    return starArray;
-  };
-
   const deleteMovie = async (id) => {
     await api.delete(`/movies/${id}`);
     window.location.href = "http://localhost:8080/";
@@ -33,21 +17,25 @@ const Movie = ({ movies }) => {
     <>
       {movies?.map((movie) => (
         <tr className="movie" key={movie.id}>
-          <td className="movie_date">
+          <td className="movie_date" data-label="DATE">
             {new Date(movie.date).toLocaleDateString()}
           </td>
-          <td className="movie_title">{movie.name}</td>
-          <td className="movie_viewer">{movie.viewer}</td>
-          <td className="movie_score">
-            <div className="star-white">
-              {[...Array(5)].map((star, index) => {
-                return <FaStar key={"white" + index} />;
-              })}
-            </div>
-            <div className="star-yellow">{getRating(movie, movie.score)}</div>
+          <td className="movie_title" data-label="TITRE">
+            {movie.name}
           </td>
+          <td className="movie_viewer" data-label="VU PAR">
+            {movie.viewer}
+          </td>
+          <td
+            style={{ "--rating": `${movie.score}` }}
+            className="movie_score"
+            data-label="NOTE"
+          ></td>
           <td>
-            <FaTrash cursor={"pointer"} onClick={(e)=>deleteMovie(movie.id)} />
+            <FaTrash
+              cursor={"pointer"}
+              onClick={(e) => deleteMovie(movie.id)}
+            />
           </td>
         </tr>
       ))}
