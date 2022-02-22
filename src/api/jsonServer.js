69673ed64f62ab4f2fa2457d5dede872fb94953d
Maxime <trillot.maxime@outlook.fr>
2022-02-22 18:15:35 +0100

    call heroku api

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://carnet-json.herokuapp.com',
   });
 
   export default instance;
