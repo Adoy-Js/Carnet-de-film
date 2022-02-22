0b1a1a3cbd4f5e1184ba803d8b5ed855054313a3
Maxime <trillot.maxime@outlook.fr>
2022-02-22 15:58:38 +0100

    update package.json

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,38 +1,38 @@
-const paths = require('./paths');
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const paths = require("./paths");
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 const port = 8080;
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   module: {
     rules: [
       // Styles
       {
         test: /\.(s?css)$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: true,
               importLoaders: 2,
             },
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               sourceMap: true,
             },
           },
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sourceMap: true,
-              implementation: require('sass'),
+              implementation: require("sass"),
             },
           },
         ],
@@ -41,17 +41,21 @@ module.exports = merge(common, {
   },
 
   devServer: {
+    client: {
+      overlay: true,
+    },
     historyApiFallback: true,
-    contentBase: paths.build,
-    clientLogLevel: 'warn',
-    overlay: true,
-    stats: 'minimal',
+    static: paths.build,
+    client: {
+      logging: "warn",
+    },
     open: false,
     compress: true,
     hot: true,
-    watchOptions: {
-      ignored: /node_modules/,
-    },
     port,
   },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  stats: "minimal",
 });
